a56aa6c40b2d69a7946f34a33af64814507fe9d6
Rohit Dham <rdhammpls@gmail.com>
2025-05-20 12:25:31 -0500

    UI Design changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,7 +5,39 @@
 $(document).ready(function () {
     console.log("Hello World!");
     $('.EditJob').hide();
-})
+    switch ($('#job_status').text) {
+        case "REJECTED":
+            $('.Job_Container').css('background', '#EF4444'); 
+            break;
+        case "WAITLISTED":
+            $('.Job_Container').css('background', '#F97316'); 
+            break;
+        case "APPLIED":
+            $('.Job_Container').css('background', '#FACC15'); 
+            break;
+        case "OA":
+            $('.Job_Container').css('background', '#A3E635'); 
+            break;
+        case "PHONE_SCREEN":
+            $('.Job_Container').css('background', '#86EFAC'); 
+            break;
+        case "INTERVIEW":
+            $('.Job_Container').css('background', '#4ADE80'); 
+            break;
+        case "OFFER":
+            $('.Job_Container').css('background', '#22C55E'); 
+            break;
+        case "ACCEPTED":
+            $('.Job_Container').css('background', '#16A34A'); 
+            break;
+        case "UNKNOWN":
+            $('.Job_Container').css('background', '#D1D5DB');
+            break;
+        default:
+            $('.Job_Container').css('background', '#E5E7EB'); 
+            break;
+    }
+});
 
 $('#addJobs').click(function () {
     console.log("Add");
@@ -24,8 +56,43 @@ function editJobWithId(jobId) {
     $("#" + jobId).show();
 }
 
-
+$('#SubmitEdit').click(function () {
+    switch ($('#job_status').text) {
+        case "REJECTED":
+            $('.Job_Container').css('background', '#EF4444');
+            break;
+        case "WAITLISTED":
+            $('.Job_Container').css('background', '#F97316');
+            break;
+        case "APPLIED":
+            $('.Job_Container').css('background', '#FACC15');
+            break;
+        case "OA":
+            $('.Job_Container').css('background', '#A3E635');
+            break;
+        case "PHONE_SCREEN":
+            $('.Job_Container').css('background', '#86EFAC');
+            break;
+        case "INTERVIEW":
+            $('.Job_Container').css('background', '#4ADE80');
+            break;
+        case "OFFER":
+            $('.Job_Container').css('background', '#22C55E');
+            break;
+        case "ACCEPTED":
+            $('.Job_Container').css('background', '#16A34A');
+            break;
+        case "UNKNOWN":
+            $('.Job_Container').css('background', '#D1D5DB');
+            break;
+        default:
+            $('.Job_Container').css('background', '#E5E7EB');
+            break;
+    }
+})
 
 //$('#delete').click(function () {
 //    console.log("Delete");
 //});
+
+ 
